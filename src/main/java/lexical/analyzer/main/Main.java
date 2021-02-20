99d1aaf29fb76a27755b6ba0aec7ab018de4fef8
Uellington Damasceno <uellington99@gmail.com>
2021-02-20 05:33:59 -0300

    Hello World.

---
+++ src/main/java/lexical/analyzer/main/Main.java
@@ -0,0 +1,11 @@
+package lexical.analyzer.main;
+
+/**
+ *
+ * @author Uellington Damasceno
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
